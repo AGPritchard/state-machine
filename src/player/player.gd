91a8a31b63e7f45874e0234718ec635ed42c0da3
Alex Pritchard <alexander.g.pritchard@gmail.com>
2020-11-27 23:49:07 +0000

    Add state machine boilerplate

--- src/player/player.gd
+++ src/player/player.gd
@@ -1,2 +1,18 @@
 extends KinematicBody2D
 
+enum STATES {
+	IDLE,
+	RUN,
+	JUMP,
+}
+
+var state: int = STATES.IDLE
+
+func _physics_process(_delta: float) -> void:
+	match state:
+		STATES.IDLE:
+			pass
+		STATES.RUN:
+			pass
+		STATES.JUMP:
+			pass
