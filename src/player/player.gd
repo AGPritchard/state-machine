e4053705c3a5a1bc67dca155c1139e3219e6f76d
Alex Pritchard <alexander.g.pritchard@gmail.com>
2020-11-28 10:08:47 +0000

    Update comments

--- src/player/player.gd
+++ src/player/player.gd
@@ -73,7 +73,7 @@ func _physics_process(_delta: float) -> void:
 			$AnimatedSprite.play("run")
 			
 		STATES.JUMP:
-			# switch to 'idle' state if maximum jump height is reached or a collision with a ceiling occurs
+			# switch to 'fall' state if maximum jump height is reached or a collision with a ceiling occurs
 			if global_position.y <= (height_before_jump - maximum_jump_height) or is_on_ceiling():
 				_switch_state(STATES.FALL)
 				gravity = gravity_strength
@@ -151,7 +151,7 @@ func _switch_state(new_state: int) -> void:
 			$StateLabel.text = "Sliding"
 
 func _on_HoldTimer_timeout() -> void:
-	# on timeout if the state is still 'hold' then switch to the 'fall' state
+	# on timeout if the state is still 'hold' then switch to the 'slide' state
 	if state == STATES.HOLD:
 		_switch_state(STATES.SLIDE)
 		gravity = gravity_strength
